30bb616d4aa23fe313eb95e33b3d1bd66e47a587
lvsi <15239928381@163.com>
2022-07-07 02:02:38 +0800

    chore: 修改CI-27

--- .github/workflows/deploy/code_check.sh
+++ .github/workflows/deploy/code_check.sh
@@ -14,10 +14,11 @@ else
 fi
 
 # 2、Check the golangci-lint version
-echo "2." $(golangci-lint --version)
+# echo "2." $(golangci-lint --version)
+echo "2." $($(go env GOPATH)/bin/golangci-lint --version)
 
 # 3、Run the golangci-lint
-golangci-lint run
+$(go env GOPATH)/bin/golangci-lint run
 if [ $? -ne 0 ]; then
   echo -e " >>>>>>>>>>>> Sorry, check code failed <<<<<<<<<<<<"
   exit 1
