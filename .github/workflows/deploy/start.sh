87c6bd6b21f0fcc5fd7db5d4ecbf68d0aec8e9bb
lvsi <15239928381@163.com>
2022-07-07 01:58:31 +0800

    chore: 修改CI-26

--- .github/workflows/deploy/start.sh
+++ .github/workflows/deploy/start.sh
@@ -1,4 +1,6 @@
 echo -e "------------ The script start.sh is running ------------"
 cd ../../../ # The base directory
 
+echo $(go env)
+
 echo -e "------------ The script start.sh is stopped ------------"
