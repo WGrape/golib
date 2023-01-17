3e9d915d46b4ab50a8533ecbddebd690dcd84d74
lvsi <15239928381@163.com>
2023-01-17 11:59:44 +0800

    style: fix test style

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -8,10 +8,7 @@ import (
 func TestRange(t *testing.T) {
 	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	res := Range(slice, func(n int) bool {
-		if n%2 != 0 {
-			return false
-		}
-		return true
+		return n%2 == 0
 	})
 	if !reflect.DeepEqual(res, []int{1, 3, 5, 7, 9}) {
 		t.Fail()
